9e7535443fb27372513e2767cc7016d9533b158a
jgab-commits <jgabalipio@gmail.com>
2026-04-14 13:52:24 +0800

    Add files via upload

---
+++ HealthSync/indexscript.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Access the form element
+    const loginForm = document.querySelector('form');
+
+    // Add an event listener for the form submission
+    loginForm.addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        const email = document.querySelector('input[type="email"]').value;
+        const password = document.querySelector('input[type="password"]').value;
+
+        if (email.trim() === '' || password.trim() === '') {
+            alert('Please enter both email and password.');
+            return;
+        }
+
+        
+        // Redirect to home page
+        window.location.href = 'admin-dashboard.html';
+    });
+});
